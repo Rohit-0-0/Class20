a59caa9cf1781949b66255e4f5cb72d988a0dfc7
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-09-04 14:54:53 +0530

    neww change

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let a = document.getElementById("heading1")
+// let a = document.getElementById("heading1")
 // console.log(a)
 // a.innerText = "Hello World";
 // console.log(a)
-let b= document.getElementsByClassName("class1")
+// let b= document.getElementsByClassName("class1")
 // b[0].removeAttribute()
-console.log(b[0].innerHTML);
+// console.log(b[0].innerHTML);
 // console.log(b[0])
+
+
+
+//for accessin single element
+let sele = document.getElementById("heading-one")
+console.log(sele)
+
+console.log(sele.innerText)
+console.log(sele.textContent)
