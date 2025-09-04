fe274ac05b85126e64ec3d67c7eb5ea84ee9f391
Rohit-0-0 <rohitpatel90878@gmail.com>
2025-09-04 14:11:02 +0530

    forth commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let a = document.getElementById("heading1")
-console.log(a)
-a.innerText = "Hello World";
-console.log(a)
+// console.log(a)
+// a.innerText = "Hello World";
+// console.log(a)
+let b= document.getElementsByClassName("class1")
+// b[0].removeAttribute()
+console.log(b[0].innerHTML);
+// console.log(b[0])
